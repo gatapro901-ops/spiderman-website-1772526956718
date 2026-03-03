995f26d2442adde0a823c1585562c5b5b3600c6e
gatapro901-ops <gatapro901@gmail.com>
2026-03-03 11:36:01 +0300

    Initial deploy: tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mode: 'jit',
+  purge: ['./index.html', './src/**/*.{js,ts,jsx,tsx}', './public/index.html'],
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
